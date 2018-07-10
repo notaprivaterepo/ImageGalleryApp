e1ee2c7456c7c465f6f17e5734466acd2041a0da
Nikita Lezya <nikitalezya@gmail.com>
2018-07-10 17:14:12 +0300

    Initial commit

---
+++ ImageGalleryApp/PageController.swift
@@ -0,0 +1,13 @@
+//
+//  PageController.swift
+//  ImageGalleryApp
+//
+//  Created by John Dowland on 10.07.2018.
+//  Copyright © 2018 Nikita Lezya. All rights reserved.
+//
+
+import UIKit
+
+class PageController: UIView {
+	
+}
